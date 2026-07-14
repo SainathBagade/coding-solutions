6ca2dd03953c46f2bba9c3c02071d79240dba532
Sainath Bagade <bagadesai4@gmail.com>
2026-07-14 10:02:04 +0530

    ✅ Find if Path Exists in Graph | Easy | C++ | Runtime: 151 ms (90.69%) | Memory: 243 MB | 2026-07-14

---
+++ leetcode/easy/1971-find-if-path-exists-in-graph/solution.cpp
@@ -0,0 +1,66 @@
+class DisjonintSet {
+    vector<int> rank, parent, size;
+
+public:
+    DisjonintSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1, 0);
+        size.resize(n + 1, 0);
+
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findParent(int node) {
+        if (node == parent[node]) {
+            return node;
+        }
+        return parent[node] = findParent(parent[node]);
+    }
+    void unionByRank(int u, int v) {
+        int ult_u = findParent(u);
+        int ult_v = findParent(v);
+        if (ult_u == ult_v)
+            return;
+
+        if (rank[ult_u] < rank[ult_v]) {
+            parent[ult_u] = ult_v;
+        } else if (rank[ult_v] < rank[ult_u]) {
+            parent[ult_v] = ult_u;
+        } else {
+            parent[ult_v] = ult_u;
+            rank[ult_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ult_u = findParent(u);
+        int ult_v = findParent(v);
+        if (ult_u == ult_v)
+            return;
+
+        if (size[ult_u] < size[ult_v]) {
+            parent[ult_u] = ult_v;
+            size[ult_v] += size[ult_u];
+        } else {
+            parent[ult_v] = ult_u;
+            size[ult_u] += size[ult_v];
+        }
+    }
+};
+
+
+class Solution {
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        DisjonintSet ds(n);
+
+         for(auto it:edges){
+             ds.unionBySize(it[0], it[1]);
+         }
+
+         return ds.findParent(source)==ds.findParent(destination);
+    }
+};

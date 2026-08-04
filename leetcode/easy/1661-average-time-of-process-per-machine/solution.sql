72bbc5a4fd9811b795ea98778cee5ba55afd928f
Sainath Bagade <bagadesai4@gmail.com>
2026-08-04 14:47:13 +0530

    ✅ Average Time of Process per Machine | Easy | SQL | Runtime: 327 ms (10.93%) | Memory: 0B | 2026-08-04

--- leetcode/easy/1661-average-time-of-process-per-machine/solution.sql
+++ leetcode/easy/1661-average-time-of-process-per-machine/solution.sql
@@ -4,5 +4,6 @@ from Activity a1
 inner join Activity a2
 on a1.process_id=a2.process_id
 AND a1.machine_id=a2.machine_id 
-AND a1.timestamp < a2.timestamp
+AND a1.activity_type = 'start'
+AND a2.activity_type = 'end'
 group by a1.machine_id

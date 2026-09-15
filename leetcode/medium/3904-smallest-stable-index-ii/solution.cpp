6610ff4a1ef2ecbda6c21455c3d6670aae987096
Sainath Bagade <bagadesai4@gmail.com>
2026-09-15 15:13:41 +0530

    ✅ Smallest Stable Index II | Medium | C++ | Runtime: 19 ms (15.43%) | Memory: 202.7 MB | 2026-09-15

---
+++ leetcode/medium/3904-smallest-stable-index-ii/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+         vector<int>temp(nums.size(),0);
+        int maxi=INT_MIN,mini=INT_MAX;
+        for(int i=0;i<nums.size();i++){
+            maxi=max(maxi,nums[i]);
+            temp[i]+=maxi;
+        }
+
+         for(int i=nums.size()-1;i>=0;i--){
+            mini=min(mini,nums[i]);
+            temp[i]-=mini;
+        }
+
+        for(int i=0;i<temp.size();i++){
+            if(temp[i] <= k) return i;
+        }
+
+        return -1;
+    }
+};
